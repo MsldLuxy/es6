83c01548f399ccd5ecd3edee0576853a71b7371e
MsldLuxy <310870966@qq.com>
2018-08-19 16:35:11 +0800

    testCode finish

---
+++ testCode/index.js
@@ -0,0 +1,15 @@
+let A = 123;
+let test = function () {
+    console.log('function');
+}
+class hello{
+    hello(){
+        console.log('class');
+    }
+}
+
+export default{
+    A,
+    test,
+    hello
+}
